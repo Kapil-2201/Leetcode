78c6fc5bcd65bb619951da42d24c0ff5cb0b2cbe
KAPIL D <kapildhanasekaran@gmail.com>
2024-07-26 10:26:58 +0530

    Time: 7 ms (10.2%), Space: 9.3 MB (6.19%) - LeetHub

---
+++ 2812-find-the-maximum-achievable-number/2812-find-the-maximum-achievable-number.c
@@ -0,0 +1,16 @@
+int theMaximumAchievableX(int num, int t){
+// int c = 0; 
+// while( num != t){
+//     if(num > t){
+//     t++;
+//     num--;}
+//     else{
+//         t--;
+//         num++;
+//     }
+
+//     c++;
+// }
+// return c;
+return num+2*t;
+}
