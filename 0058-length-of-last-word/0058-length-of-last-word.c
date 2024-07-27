c364cd0e2d2c28045ea3cfbef6d1a9999cad75ae
KAPIL D <kapildhanasekaran@gmail.com>
2024-07-27 19:14:46 +0530

    Time: 4 ms (6.28%), Space: 7.9 MB (16.16%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.c
@@ -0,0 +1,13 @@
+int lengthOfLastWord(char* s) {
+    char*token = strtok(s," ");
+   
+    char*lastword = NULL;
+    while(token != NULL){
+        
+        lastword = token;
+        token = strtok(NULL," ");
+    }
+
+    return strlen(lastword);
+
+}
