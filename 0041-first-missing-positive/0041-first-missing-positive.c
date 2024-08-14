1ad8f410bc15040e024e43a63db4dda1dcaf294d
KAPIL D <kapildhanasekaran@gmail.com>
2024-08-14 12:57:03 +0530

    Time: 63 ms (13.12%), Space: 14 MB (14.44%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.c
@@ -0,0 +1,18 @@
+int firstMissingPositive(int* nums, int n) {
+
+    for (int i = 0; i < n; i++) {
+        while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+            int temp = nums[i];
+            nums[i] = nums[temp - 1];
+            nums[temp - 1] = temp;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nums[i] != i + 1) {
+            return i + 1;
+        }
+    }
+
+    return n + 1;
+}
