12434cf6d0d8e72bf0e44ce4973114ab776d6a48
KAPIL D <kapildhanasekaran@gmail.com>
2024-08-16 10:26:45 +0530

    Time: 8 ms (84.37%), Space: 9.4 MB (15.18%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.c
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.c
@@ -5,15 +5,7 @@ int* twoSum(int* nums, int n, int target, int* returnSize) {
     int i = 0;
     *returnSize = 2;
     int * arr = (int*)malloc(*returnSize * sizeof(int));
-    // for(int i = 0;i < n;i++){
-    //     for(int j = i+1; j < n;j++){
-    //         if(nums[i] + nums[j] == target){
-    //             arr[0] = i +1 ;
-    //             arr[1] = j +1 ;
-    //             return arr;
-    //         }
-    //     }
-    // }
+
     n--;
      while(i < n){
         int check = nums[i]+nums[n];
