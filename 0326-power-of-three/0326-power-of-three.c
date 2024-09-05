9e2365d54e0488a1bbb2e3064bf157712c77ca2b
KAPIL D <kapildhanasekaran@gmail.com>
2024-09-05 14:45:36 +0530

    Time: 3 ms (98.78%), Space: 8.5 MB (12.45%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.c
+++ 0326-power-of-three/0326-power-of-three.c
@@ -1,4 +1,15 @@
 bool isPowerOfThree(int n) {
-    if(n <= 1) return n==1;
-    return n % 3 == 0 && isPowerOfThree(n/3);
+    long long power = 3;
+    if(n<=0) return false;
+    if(n == 1 || n == 3) return true;
+    if(n % 3 == 0){
+        while(n >= power){
+            if(power == n){
+                return true;
+            }
+            power*=3;
+        }
+    }
+
+    return false;
 }
